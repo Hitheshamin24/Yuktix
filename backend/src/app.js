3d7c94ecabde8b5fd5795f26587963a0b15e7252
Hithesh <hitheshamin423@gmail.com>
2026-03-02 20:45:26 +0530

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
-const cookieParser=require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 // require all the routes here
 const authRouter = require("./routes/auth.routes");
