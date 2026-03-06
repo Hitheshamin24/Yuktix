ac7617d7646c10d1b4bce60467fe0ce27ee99168
Hithesh <hitheshamin423@gmail.com>
2026-03-06 13:14:00 +0530

    add interview reports controllers

--- backend/src/controllers/interview.controller.js
+++ backend/src/controllers/interview.controller.js
@@ -1,9 +1,13 @@
 const pdfParse = require("pdf-parse");
 const generateInterviewReport = require("../services/ai.service");
 const interviewReportModel = require("../models/interviewReport.model");
-async function generateInterviewReportController(req, res) {
-  const resumeFile = req.file;
 
+/**
+ *
+ *
+ * @description controller to generate interview report based on user self description resume job
+ */
+async function generateInterviewReportController(req, res) {
   const resumeContent = await new pdfParse.PDFParse(
     Uint8Array.from(req.file.buffer),
   ).getText();
@@ -50,4 +54,42 @@ async function generateInterviewReportController(req, res) {
     interviewReport,
   });
 }
-module.exports = { generateInterviewReportController };
+
+async function getInterviewReportByIdController(res, req) {
+  const interviewId = req.params;
+  const interviewReport = await interviewReportModel.findOne({
+    _id: interviewId,
+    user: req.user.id,
+  });
+
+  if (!interviewReport) {
+    return res.status(404).json({ message: "interview report not found" });
+  }
+  res.status(200).json({
+    message: "Interview report fetched successfully",
+    interviewReport,
+  });
+}
+
+/**
+ * @description controller to get all interview report of logged in user
+ *
+ *
+ */
+async function getAllInterviewReportsController(req, res) {
+  const interviewReports = await interviewReportModel
+    .find({ user: req.user.id })
+    .sort({ createdAt: -1 })
+    .select("-resume -selfDescription -jobDescription -technicalQuestions -behavioralQuestions -skillGaps -preparationPlan");
+    
+  res.status(200).json({
+    message: "Interview reports fetched successfully",
+    interviewReports,
+  });
+
+}
+module.exports = {
+  generateInterviewReportController,
+  getInterviewReportByIdController,
+  getAllInterviewReportsController,
+};
